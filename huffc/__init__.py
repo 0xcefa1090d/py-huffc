dba5a91502870c8ea4d8267fc714ef993948bfa6
0xcefa1090d <126766757+0xcefa1090d@users.noreply.github.com>
2023-03-07 13:04:52 -0500

    Add method to fetch remote versions

--- huffc/__init__.py
+++ huffc/__init__.py
@@ -1,11 +1,23 @@
+import contextlib
 import os
 
 import requests
+import semantic_version as semver
 
 
 class VersionManager:
     def __init__(self):
         self.session = None
+
+    def fetch_remote_versions(self):
+        r = self.session.get("https://api.github.com/repos/huff-language/huff-rs/releases")
+
+        versions = []
+        for release in r.json():
+            with contextlib.suppress(ValueError):
+                versions.append(semver.Version(release["name"].removeprefix("v")))
+
+        return versions
 
     def __enter__(self):
         session = requests.Session()
